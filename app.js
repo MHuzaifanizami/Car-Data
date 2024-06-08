01811a27e2339220f247b94ccc58ae762e72d633
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-06-08 16:25:50 +0500

    file created

---
+++ app.js
@@ -0,0 +1,49 @@
+
+// Swal.fire({
+//     title: "Welcome To Pak Wheels ",
+//     text: "You clicked the button!",
+//     icon: "Welcome"
+//   });
+
+swal("Welcome To Pak Wheels" , "Click Ok To Continue" ,"info")
+
+
+var cars = {
+
+    honda: {
+        city :{
+            name: "Honda City",
+            model: 2021,
+            colors: ["black", "white"],
+            fuelType : "Petrol",
+            engine : "1500CC",
+            Price : "86 - 90 Lacs",
+        },
+        civic: {
+            name: "Honda Civic",
+            model: 2022,
+            colors: ["black", "white"],
+        },
+        freed :{
+            name: "Honda Freed",
+            model: 2020,
+            colors: ["black", "white"]
+        }
+    },
+    toyota: {
+        corolla: {
+            name: "Toyota Corolla Gli",
+            model: "2000",
+            colors : ["white" , "silver"]
+        }
+
+    },
+    suzuki : {
+        cultus : {
+            name : "Suzuki Cultus",
+            model : 1999,
+            colors : ["blue" , "red"]
+        }
+    }
+
+}
